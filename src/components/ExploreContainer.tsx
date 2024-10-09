bfbe58a8589f7fa8a40ff8465dbf9b35baa55e3c
Julio Graffin <juliograffin@gmail.com>
2024-10-09 15:33:04 -0300

    initial: add initial structure

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,11 @@
-import './ExploreContainer.css';
+import "./ExploreContainer.css";
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <h1 className="text-3xl font-bold overline">Hello world!</h1>
     </div>
   );
 };
