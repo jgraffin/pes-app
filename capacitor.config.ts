0b428b84ff8a95b8f66a987592aa0988585a0a77
jota.graffin <julio.graffin@funcionarios.automatizai.com.br>
2024-10-09 09:48:30 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pes-app',
+  webDir: 'dist'
+};
+
+export default config;
